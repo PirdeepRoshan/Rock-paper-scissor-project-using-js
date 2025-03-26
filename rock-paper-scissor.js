bb38a35dff6cc7d4c3843fd61247533d91dd3f0b
PirdeepRoshan25 <pirdeeproshan2001@gmail.com>
2025-03-26 18:40:23 +0530

    rock-paper-scissor.js

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -2,132 +2,73 @@ let score = JSON.parse(localStorage.getItem('score')) || {
     wins: 0,
     losses: 0,
     ties: 0
-  };
+};
 
-  updateScoreElement();
+updateScoreElement();
 
-  /*
-  if (!score) {
-    score = {
-      wins: 0,
-      losses: 0,
-      ties: 0
-    };
-  }
-  */
-  let isautoplaying= false;
-  let intervalid;
+let isAutoPlaying = false;
+let intervalId;
 
-  // const autoPlay= () => {
-
-  // };
-  function autoPlay() {
-    if(!isautoplaying){
-        intervalid=setInterval(() => {
+function autoPlay() {
+    if (!isAutoPlaying) {
+        intervalId = setInterval(() => {
             const playerMove = pickComputerMove();
             playGame(playerMove);
-        },1000);
-        isautoplaying=true;
-      }
-      else{
-        clearInterval(intervalid);
-        isautoplaying=false;
-      }
+        }, 1000);
+        isAutoPlaying = true;
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
     }
-    
-  document.querySelector('.js-rock-button').addEventListener('click', () => {
-      playGame('rock');
-  });
+}
 
-  document.querySelector('.js-paper-button').addEventListener('click', ()=>{
-      playGame('paper');
-  });
+document.querySelector('.js-rock-button').addEventListener('click', () => playGame('rock'));
+document.querySelector('.js-paper-button').addEventListener('click', () => playGame('paper'));
+document.querySelector('.js-scissors-button').addEventListener('click', () => playGame('scissors'));
 
-  document.body.addEventListener('keydown', (event) => {
-    if (event.key === 'r') {
-      playGame('rock');
-    } else if (event.key === 'p') {
-      playGame('paper');
-    } else if (event.key === 's') {
-      playGame('scissors');
-    }
-  });
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') playGame('rock');
+    if (event.key === 'p') playGame('paper');
+    if (event.key === 's') playGame('scissors');
+});
 
-  function playGame(playerMove) {
+function playGame(playerMove) {
     const computerMove = pickComputerMove();
-
     let result = '';
 
-    if (playerMove === 'scissors') {
-      if (computerMove === 'rock') {
-        result = 'You lose.';
-      } else if (computerMove === 'paper') {
-        result = 'You win.';
-      } else if (computerMove === 'scissors') {
+    if (playerMove === computerMove) {
         result = 'Tie.';
-      }
-
-    } else if (playerMove === 'paper') {
-      if (computerMove === 'rock') {
+    } else if ((playerMove === 'rock' && computerMove === 'scissors') ||
+               (playerMove === 'paper' && computerMove === 'rock') ||
+               (playerMove === 'scissors' && computerMove === 'paper')) {
         result = 'You win.';
-      } else if (computerMove === 'paper') {
-        result = 'Tie.';
-      } else if (computerMove === 'scissors') {
+        score.wins += 1;
+    } else {
         result = 'You lose.';
-      }
-      
-    } else if (playerMove === 'rock') {
-      if (computerMove === 'rock') {
-        result = 'Tie.';
-      } else if (computerMove === 'paper') {
-        result = 'You lose.';
-      } else if (computerMove === 'scissors') {
-        result = 'You win.';
-      }
-    }
-
-    if (result === 'You win.') {
-      score.wins += 1;
-    } else if (result === 'You lose.') {
-      score.losses += 1;
-    } else if (result === 'Tie.') {
-      score.ties += 1;
+        score.losses += 1;
     }
 
     localStorage.setItem('score', JSON.stringify(score));
-    //update score element in the browser
     updateScoreElement();
 
-   //update result element in the browser
     document.querySelector('.js-result').innerHTML = result;
-    
-    //update moves element in the browser
-    document.querySelector('.js-moves').innerHTML = 
-    `You
-    <img src="images/${playerMove}.png" 
-    class="move-icon">
-    <img src="images/${computerMove}.png"
-    class="move-icon">
-    Computer`;
-  }
+    document.querySelector('.js-moves').innerHTML = `
+        You <img src="${playerMove}.png" class="move-icon">
+        <img src="${computerMove}.png" class="move-icon"> Computer
+    `;
+}
 
-  function updateScoreElement() {
-    document.querySelector('.js-score')
-      .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
-  }
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
 
-  function pickComputerMove() {
-    const randomNumber = Math.random();
+function pickComputerMove() {
+    const moves = ['rock', 'paper', 'scissors'];
+    return moves[Math.floor(Math.random() * 3)];
+}
 
-    let computerMove = '';
-
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-      computerMove = 'rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-      computerMove = 'paper';
-    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-      computerMove = 'scissors';
-    }
-
-    return computerMove;
-  }
+function resetScore() {
+    score = { wins: 0, losses: 0, ties: 0 };
+    localStorage.removeItem('score');
+    updateScoreElement();
+}
